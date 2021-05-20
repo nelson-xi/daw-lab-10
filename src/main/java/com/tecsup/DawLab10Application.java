b1fe7a67fbc8c1ce594c8af2d38199bce08906d2
Nelson Gayoso <nelson.gayoso@tecsup.edu.pe>
2021-05-19 20:10:26 -0500

    first commit

---
+++ src/main/java/com/tecsup/DawLab10Application.java
@@ -0,0 +1,13 @@
+package com.tecsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawLab10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawLab10Application.class, args);
+	}
+
+}
